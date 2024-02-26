3fbb43be5f573820ee81902ce8cdda159bd0785e
sidikmaulanaaulia <auliamaulanasidik@gmail.com>
2024-02-26 14:06:24 +0700

    modified login

--- admin/proses-login.php
+++ admin/proses-login.php
@@ -27,22 +27,27 @@ if (isset($_POST['submit'])) {
       }
 
    }
-   //tabel user_siswa
-   $sql = "SELECT * FROM tabel_siswa WHERE nis_siswa='$username'";
+      //tabel user_siswa
+   $sql = "SELECT * FROM user_siswa WHERE nis_siswa='$username'";
    $result = $conn->query($sql);
    if ($result->num_rows == 1) {
-      $row = mysqli_fetch_assoc($result);
-      if ($password == $row['password']) {
-         include 'sessions-siswa.php';
-         header("location:../siswa/index.php");
-         exit();
-      }
+     $kunci = "@43988.348";
+     $password1_input = md5($password.$kunci.$password);
+     $password2_input = md5($password1_input.$kunci.$password1_input);
+     $row = mysqli_fetch_assoc($result);
+     if ($password2_input == $row['password']) {
+      $data_siswa = mysqli_query($conn,"SELECT * FROM tabel_siswa WHERE nis_siswa = $row[nis_siswa] ");
+      $data = mysqli_fetch_array($data_siswa);
+      include 'sessions-siswa.php';
+      header("location:../siswa/index.php");
+      exit();
    }
+}
 
    // Jika username tidak ditemukan di kedua tabel
-   $_SESSION['pesan_login'] = "Login gagal. Coba lagi.";
-   header("Location:../index.php"); 
-   exit();
+$_SESSION['pesan_login'] = "Login gagal. Coba lagi.";
+header("Location:../index.php"); 
+exit();
 }
 
 ?>
--- admin/sessions-siswa.php
+++ admin/sessions-siswa.php
@@ -1,14 +1,13 @@
 <?php 
-
-$_SESSION['id_siswa'] = $row['id'];
-$_SESSION['nama_siswa'] = $row['nama_siswa'];
-$_SESSION['nis_siswa'] = $row['nis_siswa'];
-$_SESSION['id_kelas'] = $row['id_kelas'];
-$_SESSION['alamat'] = $row['alamat'];
-$_SESSION['tempat_lahir'] = $row['tempat_lahir'];
-$_SESSION['tgl_lahir'] = $row['tgl_lahir'];
-$_SESSION['agama'] = $row['agama'];
-$_SESSION['nama_ortu'] = $row['nama_ortu'];
-$_SESSION['tahun_masuk'] = $row['tahun_masuk'];
-$_SESSION['jarak_tempuh'] = $row['jarak_tempuh'];
+$_SESSION['id_siswa'] = $data['id'];
+$_SESSION['nama_siswa'] = $data['nama_siswa'];
+$_SESSION['nis_siswa'] = $data['nis_siswa'];
+$_SESSION['id_kelas'] = $data['id_kelas'];
+$_SESSION['alamat'] = $data['alamat'];
+$_SESSION['tempat_lahir'] = $data['tempat_lahir'];
+$_SESSION['tgl_lahir'] = $data['tgl_lahir'];
+$_SESSION['agama'] = $data['agama'];
+$_SESSION['nama_ortu'] = $data['nama_ortu'];
+$_SESSION['tahun_masuk'] = $data['tahun_masuk'];
+$_SESSION['jarak_tempuh'] = $data['jarak_tempuh'];
  ?>
